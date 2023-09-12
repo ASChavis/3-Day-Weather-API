2be04167e34fdda6739caee704fe365f56cba96f
ASChavis <suzycodes@gmail.com>
2023-09-11 21:36:56 -0400

    trying to solve JS

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-
-const apiKey = "be43167956334322af8222757230609";
-const apiURL = "http://api.weatherapi.com/v1/forecast.json?&days=3&aqi=no&alerts=yes&q=";
-const searchBar = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
+const searchBar = document.querySelector("input");
+const searchBtn = document.querySelector("button");
 
 async function checkWeather (city){
-    const response = await fetch(apiURL + city + `&key=${apiKey}`);
+
+    const apiKey = "&key=be43167956334322af8222757230609";
+    const apiURL = "http://api.weatherapi.com/v1/forecast.json";
+
+
+    const response = await fetch(`${apiURL}?q=${city}${apiKey}&days=3`);
     var data = await response.json();
 
     console.log(data);
+const classes = {
+    ".city": data.location.name + ", " + data.location.region,
+    ".day1": data.forecast.forecastday[0].date,
+    ".day2": data.forecast.forecastday[1].date, 
+    ".day3": data.forecast.forecastday[2].date, 
+    ".temp1": data.forecast.forecastday[0].day.avgtemp_f + " °F", 
+    ".temp2": data.forecast.forecastday[1].day.avgtemp_f + " °F", 
+    ".temp3": data.forecast.forecastday[2].day.avgtemp_f + " °F", 
+    ".condition1": data.forecast.forecastday[0].day.condition["text"],
+    ".condition2": data.forecast.forecastday[1].day.condition["text"], 
+    ".condition3": data.forecast.forecastday[2].day.condition["text"], 
+    ".percentRain1": "Chance for Rain: " + data.forecast.forecastday[0].day.daily_chance_of_rain + "%",
+    ".percentRain2": "Chance for Rain: " + data.forecast.forecastday[1].day.daily_chance_of_rain + "%", 
+    ".percentRain3": "Chance for Rain: " + data.forecast.forecastday[2].day.daily_chance_of_rain + "%", 
+    ".uv1": "UV: " + data.forecast.forecastday[0].day.uv, 
+    ".uv2": "UV: " + data.forecast.forecastday[1].day.uv, 
+    ".uv3": "UV: " + data.forecast.forecastday[2].day.uv, 
+    ".moonphase1": data.forecast.forecastday[0].astro.moon_phase,
+    ".moonphase2": data.forecast.forecastday[1].astro.moon_phase, 
+    ".moonphase3": data.forecast.forecastday[2].astro.moon_phase, 
+    ".coordinates": "Lat " + data.location.lat + ", Lon " + data.location.lon,
+};
 
-document.querySelector(".city").innerHTML = data.location.name + ", " + data.location.region;
-document.querySelector(".day1").innerHTML = data.forecast.forecastday[0].date;
-document.querySelector(".day2").innerHTML = data.forecast.forecastday[1].date;
-document.querySelector(".day3").innerHTML = data.forecast.forecastday[2].date;
-document.querySelector(".temp1").innerHTML = data.forecast.forecastday[0].day.avgtemp_f + " °F";
-document.querySelector(".temp2").innerHTML = data.forecast.forecastday[1].day.avgtemp_f + " °F";
-document.querySelector(".temp3").innerHTML = data.forecast.forecastday[2].day.avgtemp_f + " °F";
-document.querySelector(".condition1").innerHTML = data.forecast.forecastday[0].day.condition["text"];
-document.querySelector(".condition2").innerHTML = data.forecast.forecastday[1].day.condition["text"];
-document.querySelector(".condition3").innerHTML = data.forecast.forecastday[2].day.condition["text"];
-document.querySelector(".percentRain1").innerHTML = "Chance for Rain: " + data.forecast.forecastday[0].day.daily_chance_of_rain + "%";
-document.querySelector(".percentRain2").innerHTML = "Chance for Rain: " + data.forecast.forecastday[1].day.daily_chance_of_rain + "%";
-document.querySelector(".percentRain3").innerHTML = "Chance for Rain: " + data.forecast.forecastday[2].day.daily_chance_of_rain + "%";
-document.querySelector(".uv1").innerHTML = "UV: " + data.forecast.forecastday[0].day.uv;
-document.querySelector(".uv2").innerHTML = "UV: " + data.forecast.forecastday[1].day.uv;
-document.querySelector(".uv3").innerHTML = "UV: " + data.forecast.forecastday[2].day.uv;
-document.querySelector(".moonphase1").innerHTML = data.forecast.forecastday[0].astro.moon_phase;
-document.querySelector(".moonphase2").innerHTML = data.forecast.forecastday[1].astro.moon_phase;
-document.querySelector(".moonphase3").innerHTML = data.forecast.forecastday[2].astro.moon_phase;
-document.querySelector(".coordinates").innerHTML = "Lat " + data.location.lat + ", Lon " + data.location.lon;
-
-
+for (let className in classes) {
+    document.querySelector(className).innerHTML = classes[className];
+}
 
 document.querySelector(".icon1").src = data.forecast.forecastday[0].day.condition.icon;
 document.querySelector(".icon2").src = data.forecast.forecastday[1].day.condition.icon;
@@ -44,6 +49,6 @@ searchBtn.addEventListener("click", function (){
 });
 
 searchBar.addEventListener("keydown", function (event){
-    if (event.keyCode === 13 || event.which === 13){
+    if (event.key === "Enter"){
     checkWeather(searchBar.value);}
 });
